0811ecbf9ce69b43a20ce06877df558c5686cad6
moha-salad64 <mohamedsalaad509@gmail.com>
2025-01-08 00:48:39 +0300

    secondy commit

--- login.php
+++ login.php
@@ -6,14 +6,34 @@
     <title>login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- main.css file link -->
-      <link rel="stylesheet" href="main.css"> 
+    <link rel="stylesheet" href="main.css"> 
 </head>
 <body>
-    <div class="container">
-        <div class="mt-4">
-          <label for="exampleFormControlInput1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-        </div> 
+    <div class="container  logincontainer">
+            <h3 class="fs-1 fw-bold text-center">Login</h3>
+            <div class="mt-2">
+              <label  class="form-label">Username</label>
+              <input type="text" class="form-control" name ="username" placeholder="Enter username">
+            </div> 
+            <div class="mt-2">
+              <label class="form-label">Password</label>
+              <input type="password" class="form-control" name="password" placeholder="Enter Password">
+            </div> 
+            <div class="mt-2 d-flex justify-content-between">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault"  unchecked>
+                    <label class="form-check-label" > Remember me </label>
+                </div>
+                <div class="">
+                    <p>
+                        <span> <a class="link-offset-2 link-underline link-underline-opacity-0" href="#">Forget password</a></span>
+                    </p>
+                </div>
+            </div>
+        <div class="mt">
+            <button type="button" class="btn btn-success p-1  w-50">Log in</button>
+            <button type="button" class="btn btn-danger  w-50">Cancel</button>
+        </div>
     </div>
 
 

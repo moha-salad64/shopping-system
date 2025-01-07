f83e1bf83d3f41267ac7d6a3577b7970596b8add
moha-salad64 <mohamedsalaad509@gmail.com>
2025-01-07 17:54:24 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping System</title>
+</head>
+<body>
+<?php
+require ("include_files/header.html");
+require ("include_files/main.html");
+require ("include_files/footer.html");
+?>
+</body>
+</html>
